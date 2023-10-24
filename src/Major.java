0d26ad9337eb73d4cba48f2615c6dacbb9682921
Eden-Wang1710 <2974709422@qq.com>
2023-10-24 10:38:04 +0800

    cao

---
+++ src/Major.java
@@ -0,0 +1,11 @@
+public class Major {
+    private String name;
+    public Major(String name)
+    {
+        this.name = name;
+    }
+    public String getMajor()
+    {
+        return name;
+    }
+}
