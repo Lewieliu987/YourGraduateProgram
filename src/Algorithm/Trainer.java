54f8fbdbe843fd50d0cab076a84678d0ec9801a7
Eden-Wang1710 <2974709422@qq.com>
2023-10-24 12:26:13 +0800

    zixuanw

--- src/Algorithm/Trainer.java
+++ src/Algorithm/Trainer.java
@@ -1,4 +1,8 @@
 package Algorithm;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 import Other.Datapoint;
@@ -7,6 +11,28 @@ public class Trainer extends NaiveBayes{
     private ArrayList<Datapoint> database;
 
     public Trainer(){
-        
+        File csv = new File(filePath);
+        csv.setReadable(true);
+        csv.setWritable(true);
+        InputStreamReader isr = null;
+        BufferedReader br = null;
+        try {
+            isr = new InputStreamReader(new FileInputStream(csv), "UTF-8");
+            br = new BufferedReader(isr);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        String line = "";
+        ArrayList<String> records = new ArrayList<>();
+        try {
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+                records.add(line);
+            }
+            System.out.println("csv表格读取行数：" + records.size());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
