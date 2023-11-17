e20fd74147baed0dd471fad1b9c996e64c743fcf
Eden-Wang1710 <2974709422@qq.com>
2023-11-17 13:08:44 +0800

    zixuanw

--- src/System/Main.java
+++ src/System/Main.java
@@ -16,9 +16,9 @@ import User.UserInputStudent;
 
 public class Main {
     public static void main(String[] args) {
-        // Trainer t = new Trainer();
-        // t.trainParameters();
-        // t.updateParameters();
+        Trainer t = new Trainer();
+        t.trainParameters();
+        t.updateParameters();
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to CityU YGP System, Log in, Register?");
