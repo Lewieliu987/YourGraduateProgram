e5dba8afb5635ff72309dc0132c90f0c91845ca9
sasyktumoo <sasyktumoo@gmail.com>
2023-11-26 21:57:01 +0800

    final version

--- test/testUser/AdminTest.java
+++ test/testUser/AdminTest.java
@@ -28,7 +28,7 @@ class AdminTest {
     @Test
     void testGetId() {
         // Assuming the ID system always generates ID 1 for the first user
-        assertEquals(341011, user.getId());
+        assertEquals(341021, user.getId());
     }
 
     @Test
